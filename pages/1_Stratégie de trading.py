ed2bcc43d9206fc0c7d86feadaaa843be34831b5
Dvdzh <[votre-email@exemple.com]>
2025-04-30 22:16:02 -0400

    [part 1] nouvelles strategies

--- pages/1_Stratégie de trading.py
+++ pages/1_Stratégie de trading.py
@@ -16,35 +16,60 @@ with st.sidebar:
                             [
                                                 "RSI",
                                                 "Bollinger",
-                                                # ChaikinMoneyFlows
+                                                "ChaikinMoneyFlow",
                                                 "StochasticR",
                                                 "TDI",
-                                                # Momentum60Days",
-                                                # VXN
-                                                # Credit
-                                                # YieldCurve
-
-                                                "Momentum1Day",
-                                                "Momentum5Days",
+                                                "Momentum60Day",
+                                                "VXN",
+                                                "YieldCurve",
+                                                "CreditSpread",
                                                 ],
-                                                index=3)
+                                                index=0)
     holding_date = st.selectbox("Select Holding Date", ["1", "5"])
-# with col1:
-#     # Strategy dropdown
-#     strategy = st.selectbox("Select Strategy", ["Bollinger", 
-#                                                 "Momentum1Day", 
-#                                                 "Momentum5Days", 
-#                                                 "RSI", 
-#                                                 "TDI",
-#                                                 "StochasticR",],
-#                                                 index=3)
-# with col2:
-#     # Holding date dropdown
-#     holding_date = st.selectbox("Select Holding Date", ["1", "5"])
+
+    if strategy == "RSI":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le RSI est inférieur à 40 et vend quand il est supérieur à 60.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "Bollinger":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le prix est à l'intérieur des bandes de Bollinger (0 ≤ pctB ≤ 1)")
+            st.write("Vend quand il est au-dessus de la bande supérieure (pctB > 1).")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "ChaikinMoneyFlow":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le CMF est supérieur à 0.20 et vend quand il est inférieur à -0.20.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "StochasticR":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le %R est inférieur à 50 et vend quand il est supérieur à 90.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "TDI":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le TDI est supérieur à 0 et le DI est supérieur à 0.")
+            st.write("Vend quand le TDI est supérieur à 0 et le DI est inférieur à 0.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "Momentum60Day":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le prix est supérieur à celui d'il y a 60 jours.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "VXN":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le VXN est supérieur à 30.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "YieldCurve":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le Yield Curve est supérieur à 0.20.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+    elif strategy == "CreditSpread":
+        with st.expander("Explications de la stratégie", expanded=True):
+            st.write("Achete quand le Credit Spread est supérieur à 0.20.")
+            st.write(f"Vend les positions aux bout de {holding_date} jours.")
+
 
 def display_html(TICKER, strategy, holding_date):
     try:
-        with open(f"data/html_results/{TICKER}_{strategy}Strategy_holdbars{holding_date}.html", 'r', encoding='utf-8') as f:
+        with open(f"data/strategies/html_results/{TICKER}_{strategy}Strategy_holdbars{holding_date}.html", 'r', encoding='utf-8') as f:
             html_content = f.read()
         components.html(html_content, height=700, scrolling=True)
     except FileNotFoundError:
@@ -53,7 +78,7 @@ def display_html(TICKER, strategy, holding_date):
 def display_info(TICKER, strategy, holding_date):
     try:
         # read csv 
-        stats_df = pd.read_csv(f"data/csv_results/{TICKER}_{strategy}Strategy_holdbars{holding_date}_stats.csv", index_col=0)
+        stats_df = pd.read_csv(f"data/strategies/csv_results/{TICKER}_{strategy}Strategy_holdbars{holding_date}_stats.csv", index_col=0)
 
         start = stats_df.loc["Start"].iloc[0][:-9]
         end = stats_df.loc["End"].iloc[0][:-9]
@@ -71,6 +96,20 @@ def display_info(TICKER, strategy, holding_date):
         profit_factor = round(float(stats_df.loc["Profit Factor"].iloc[0]), 2)
         expectancy = round(float(stats_df.loc["Expectancy [%]"].iloc[0]), 2)
 
+        stats_mean_df = pd.read_csv(f"data/strategies/csv_results/{TICKER}_mean_holdbars{holding_date}.csv", index_col=0)
+        return_info_mean = round(float(stats_mean_df.loc["Return [%]"].iloc[0]), 2)
+        return_ann_mean = round(float(stats_mean_df.loc["Return (Ann.) [%]"].iloc[0]), 2)
+        volatility_ann_mean = round(float(stats_mean_df.loc["Volatility (Ann.) [%]"].iloc[0]), 2)
+        sharpe_ratio_mean = round(float(stats_mean_df.loc["Sharpe Ratio"].iloc[0]), 2)
+        sortino_ratio_mean = round(float(stats_mean_df.loc["Sortino Ratio"].iloc[0]), 2)
+
+        max_drawdown_mean = round(float(stats_mean_df.loc["Max. Drawdown [%]"].iloc[0]), 2)
+        trades_mean = int(stats_mean_df.loc["# Trades"].iloc[0])  # Si c'est un entier, on le garde comme tel
+        win_rate_mean = round(float(stats_mean_df.loc["Win Rate [%]"].iloc[0]), 2)
+        profit_factor_mean = round(float(stats_mean_df.loc["Profit Factor"].iloc[0]), 2)
+        expectancy_mean = round(float(stats_mean_df.loc["Expectancy [%]"].iloc[0]), 2)
+
+
         st.info(
             f"""
             **Start :** {start} \n
@@ -82,25 +121,25 @@ def display_info(TICKER, strategy, holding_date):
         subcol1, subcol2 = st.columns(2)
         with subcol1:
             # return, return ann, volatility ann, sharpe ratio, sortino ratio
-            st.metric(label="Return [%]", value=return_info, delta="-0.01%")
-            st.metric(label="Return (Ann.) [%]", value=return_ann, delta="-0.01%")
-            st.metric(label="Volatility (Ann.) [%]", value=volatility_ann, delta="0.01%")
-            st.metric(label="Sharpe Ratio", value=sharpe_ratio, delta="0.01")
-            st.metric(label="Sortino Ratio", value=sortino_ratio, delta="0.01")
+            st.metric(label="Return [%]", value=return_info, delta=round(float(return_info-return_info_mean), 2))
+            st.metric(label="Return (Ann.) [%]", value=return_ann, delta=round(float(return_ann-return_ann_mean), 2))
+            st.metric(label="Volatility (Ann.) [%]", value=volatility_ann, delta=round(float(volatility_ann-volatility_ann_mean), 2))
+            st.metric(label="Sharpe Ratio", value=sharpe_ratio, delta=round(float(sharpe_ratio-sharpe_ratio_mean), 2))
+            st.metric(label="Sortino Ratio", value=sortino_ratio, delta=round(float(sortino_ratio-sortino_ratio_mean), 2))
         with subcol2:
             # max drawdown, trades, win rate, profit factor, expectancy
-            st.metric(label="Max Drawdown [%]", value=max_drawdown, delta="-0.01%")
-            st.metric(label="Trades", value=trades, delta="0")
-            st.metric(label="Win Rate [%]", value=win_rate, delta="0.01%")
-            st.metric(label="Profit Factor", value=profit_factor, delta="0.01")
-            st.metric(label="Expectancy [%]", value=expectancy, delta="0.01%")
+            st.metric(label="Max Drawdown [%]", value=max_drawdown, delta=round(float(max_drawdown-max_drawdown_mean), 2))
+            st.metric(label="Trades", value=trades, delta=trades - trades_mean)
+            st.metric(label="Win Rate [%]", value=win_rate, delta=round(float(win_rate-win_rate_mean), 2))
+            st.metric(label="Profit Factor", value=profit_factor, delta=round(float(profit_factor-profit_factor_mean), 2))
+            st.metric(label="Expectancy [%]", value=expectancy, delta=round(float(expectancy-expectancy_mean), 2))
 
     except FileNotFoundError:
         st.error(f"Error: {TICKER} backtest results file not found.")
 
 def display_trades(TICKER, strategy, holding_date):
     try:
-        trades_df = pd.read_csv(f"data/csv_results/{TICKER}_{strategy}Strategy_holdbars{holding_date}_trades.csv", index_col=0)
+        trades_df = pd.read_csv(f"data/strategies/csv_results/{TICKER}_{strategy}Strategy_holdbars{holding_date}_trades.csv", index_col=0)
         # drop SL, TP, Tag
         trades_df = trades_df.drop(columns=["SL", "TP", "Tag"])
         st.dataframe(trades_df, use_container_width=True)
